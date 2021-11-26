8df930b972756f90c224993ce9d3f3d0be32bc79
pinocchio22 <vviian.2@gmail.com>
2021-11-26 17:11:57 +0900

    feat : firebase
    connecting firebase

--- app/src/main/java/com/example/p2glet_sns/LoginActivity.kt
+++ app/src/main/java/com/example/p2glet_sns/LoginActivity.kt
@@ -2,8 +2,12 @@ package com.example.p2glet_sns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    var auth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
