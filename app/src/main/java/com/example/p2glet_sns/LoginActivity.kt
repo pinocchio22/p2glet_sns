39d59c8f59c603657f31f3034d04c30f5831c077
pinocchio22 <vviian.2@gmail.com>
2021-11-26 17:20:05 +0900

    feat : LoginActivity.kt
    added fun signin..()

--- app/src/main/java/com/example/p2glet_sns/LoginActivity.kt
+++ app/src/main/java/com/example/p2glet_sns/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.p2glet_sns
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -11,5 +12,32 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        auth = FirebaseAuth.getInstance()
     }
+
+    fun signinAndSignup() {
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())?.addOnCompleteListener {
+            task ->
+            if (task.isSuccessful) {
+                //Creating a user account
+            }else if (task.exception?.message.isNullOrEmpty()){
+                //show the error messagge
+            }else {
+                //Login if you have account
+            }
+        }
+    }
+
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())?.addOnCompleteListener {
+            task ->
+            if (task.isSuccessful) {
+                //Login
+            }else {
+                //Show the error message
+            }
+        }
+    }
+
+    fun
 }
