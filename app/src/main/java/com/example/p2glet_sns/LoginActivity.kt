1915c33684452edf850eac04aaf88f1835a1bccd
pinocchio22 <vviian.2@gmail.com>
2021-11-26 17:23:11 +0900

    feat : LoginActivity.kt
    added fun signin..()

--- app/src/main/java/com/example/p2glet_sns/LoginActivity.kt
+++ app/src/main/java/com/example/p2glet_sns/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.p2glet_sns
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -20,10 +23,13 @@ class LoginActivity : AppCompatActivity() {
             task ->
             if (task.isSuccessful) {
                 //Creating a user account
+                moveMainPage(task.result.user)
             }else if (task.exception?.message.isNullOrEmpty()){
                 //show the error messagge
+                Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
             }else {
                 //Login if you have account
+                signinEmail()
             }
         }
     }
@@ -33,11 +39,17 @@ class LoginActivity : AppCompatActivity() {
             task ->
             if (task.isSuccessful) {
                 //Login
+                moveMainPage(task.result.user)
             }else {
                 //Show the error message
+                Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
             }
         }
     }
 
-    fun
+    fun moveMainPage(user : FirebaseUser?) {
+        if (user != null) {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
 }
