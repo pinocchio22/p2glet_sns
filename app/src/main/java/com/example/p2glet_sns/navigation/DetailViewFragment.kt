5c4b2e6ba98b67fbfb287998dacf9b0dfc574eb9
pinocchio22 <vviian.2@gmail.com>
2021-11-30 21:08:27 +0900

    feat : DetailViewFragment.kt
    added DetailViewFragment.kt

---
+++ app/src/main/java/com/example/p2glet_sns/navigation/DetailViewFragment.kt
@@ -0,0 +1,20 @@
+package com.example.p2glet_sns.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2021-11-30
+ * @desc
+ */
+class DetailViewFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
