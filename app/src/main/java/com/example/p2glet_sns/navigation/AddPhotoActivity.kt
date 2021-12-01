d7e041dd92f0bd78d7866f17a500af837327f27a
pinocchio22 <vviian.2@gmail.com>
2021-12-01 14:32:25 +0900

    fix : gradle
    modified firebase-storage

--- app/src/main/java/com/example/p2glet_sns/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/p2glet_sns/navigation/AddPhotoActivity.kt
@@ -3,8 +3,11 @@ package com.example.p2glet_sns.navigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.p2glet_sns.R
+import com.google.firebase.storage.FirebaseStorage
 
 class AddPhotoActivity : AppCompatActivity() {
+    var PICK_IMAGE_FROM_ALBUM = 0
+    var storage : FirebaseStorage? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
