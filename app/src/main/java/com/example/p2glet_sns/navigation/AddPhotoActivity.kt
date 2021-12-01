bba356684ff5772adaa7b51ef93169716ce7dab7
pinocchio22 <vviian.2@gmail.com>
2021-12-01 14:35:31 +0900

    feat : AddPhotoActivity.kt
    overrode fun onCreate

--- app/src/main/java/com/example/p2glet_sns/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/p2glet_sns/navigation/AddPhotoActivity.kt
@@ -1,15 +1,34 @@
 package com.example.p2glet_sns.navigation
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.p2glet_sns.R
+import com.google.android.gms.auth.api.signin.internal.Storage
 import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
 
 class AddPhotoActivity : AppCompatActivity() {
     var PICK_IMAGE_FROM_ALBUM = 0
     var storage : FirebaseStorage? = null
+    var photoUri : Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
+
+        //Initiate store
+        storage = FirebaseStorage.getInstance()
+
+        //Open the album
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+
+        //add image upload event
+        addphoto_btn_upload.setOnClickListener {
+            contentUpload()
+        }
     }
+
 }
