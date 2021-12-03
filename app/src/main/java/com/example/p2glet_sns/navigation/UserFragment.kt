fb98fa5723f621dc59fb80a192af21c1d927eb99
pinocchio22 <vviian.2@gmail.com>
2021-12-03 21:53:30 +0900

    feat : UserFragment.kt
    made inner class UserFragmentRecyclerViewAdapter

--- app/src/main/java/com/example/p2glet_sns/navigation/UserFragment.kt
+++ app/src/main/java/com/example/p2glet_sns/navigation/UserFragment.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.p2glet_sns.R
+import com.example.p2glet_sns.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_user.view.*
 
 /**
  * @author CHOI
@@ -14,9 +19,41 @@ import com.example.p2glet_sns.R
  * @desc
  */
 class UserFragment : Fragment() {
-
+    var fragmentView : View? = null
+    var firestore : FirebaseFirestore? = null
+    var uid : String? = null
+    var auth : FirebaseAuth? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user,container,false)
         return view
+    }
+
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+       var contentDTOs : ArrayList<ContentDTO> = arrayListOf()
+        init {
+            firestore?.collection("images")?.whereEqualTo("uid",uid)?.addSnapshotListener { querySnapshot, firebaseFirestore ->
+                //Somtimes, This code return null of querySnapshot when it signout
+                if (querySnapshot == null) return@addSnapshotListener
+
+                //Get data
+                for (snapshot in querySnapshot.documents) {
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+                fragmentView?.account_tv_post_count?.text = contentDTOs.size.toString()
+                notifyDataSetChanged()
+            }
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
     }
 }
