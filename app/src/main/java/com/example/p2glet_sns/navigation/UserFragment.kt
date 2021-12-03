82dd2c2e62bdb22ff131b016b641f125a900e40f
pinocchio22 <vviian.2@gmail.com>
2021-12-03 22:00:05 +0900

    feat : UserFragment.kt
    overrode fun onCreateViewHolder, onBindViewHolder, getItemCount

--- app/src/main/java/com/example/p2glet_sns/navigation/UserFragment.kt
+++ app/src/main/java/com/example/p2glet_sns/navigation/UserFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.p2glet_sns.R
 import com.example.p2glet_sns.navigation.model.ContentDTO
 import com.google.firebase.auth.FirebaseAuth
@@ -44,15 +48,23 @@ class UserFragment : Fragment() {
             }
         }
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            TODO("Not yet implemented")
+            var width = resources.displayMetrics.widthPixels / 3
+            var imageView = ImageView(parent.context)
+            imageView.layoutParams = LinearLayoutCompat.LayoutParams(width,width)
+            return CustomViewHolder(imageView)
+        }
+
+        inner class CustomViewHolder(var imageView: ImageView) : RecyclerView.ViewHolder() {
+
         }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            TODO("Not yet implemented")
+            var imageView = (holder as CustomViewHolder).imageView
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl).apply(RequestOptions().centerCrop()).into(imageView)
         }
 
         override fun getItemCount(): Int {
-            TODO("Not yet implemented")
+            return contentDTOs.size
         }
 
     }
