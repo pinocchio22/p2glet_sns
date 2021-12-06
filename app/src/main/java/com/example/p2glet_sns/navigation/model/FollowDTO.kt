599cd5c0523a23c86aaa9132b7acf0f25fd31652
pinocchio22 <vviian.2@gmail.com>
2021-12-06 21:20:31 +0900

    feat : FollowDTO
    data class

---
+++ app/src/main/java/com/example/p2glet_sns/navigation/model/FollowDTO.kt
@@ -0,0 +1,15 @@
+package com.example.p2glet_sns.navigation.model
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2021-12-06
+ * @desc
+ */
+data class FollowDTO (
+        var followerCount : Int = 0,
+        var followers : MutableMap<String, Boolean> = HashMap(),
+
+        var followingCount : Int = 0,
+        var followings : MutableMap<String, Boolean> = HashMap()
+)
