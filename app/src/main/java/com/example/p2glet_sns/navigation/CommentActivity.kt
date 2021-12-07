5492479655e58155023753e7dbb9f7320f158793
pinocchio22 <vviian.2@gmail.com>
2021-12-07 23:42:32 +0900

    feat : layout
    item_comment.xml and activity_comment.xml

---
+++ app/src/main/java/com/example/p2glet_sns/navigation/CommentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.p2glet_sns.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.p2glet_sns.R
+
+class CommentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+    }
+}
