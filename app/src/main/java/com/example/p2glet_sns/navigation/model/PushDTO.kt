3f951f2ec664ab609eb560518bb5bd8df50053a6
pinocchio22 <vviian.2@gmail.com>
2021-12-09 15:32:49 +0900

    feat : PushDTO
    data class

---
+++ app/src/main/java/com/example/p2glet_sns/navigation/model/PushDTO.kt
@@ -0,0 +1,19 @@
+package com.example.p2glet_sns.navigation.model
+
+import android.app.Notification
+
+/**
+ * @author CHOI
+ * @email vviian.2@gmail.com
+ * @created 2021-12-09
+ * @desc
+ */
+data class PushDTO (
+        var to : String? = null,
+        var notification : Notification = Notification()
+){
+    data class Notification(
+        var body : String? = null,
+        var titile : String? = null
+    )
+}
