6dd9c61e0b60b1179ecf6d104e4adc56c2d379da
pinocchio22 <vviian.2@gmail.com>
2021-12-15 18:30:31 +0900

    fix : mapping
    message, time mapping 성공 but, name mapping 실패

--- app/src/main/java/com/example/p2glet_sns/ChatActivity.kt
+++ app/src/main/java/com/example/p2glet_sns/ChatActivity.kt
@@ -190,19 +190,15 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
-import com.example.p2glet_sns.R
-import com.example.p2glet_sns.navigation.model.AlarmDTO
 import com.example.p2glet_sns.navigation.model.ChatDTO
 import com.example.p2glet_sns.navigation.model.ContentDTO
 import com.example.p2glet_sns.navigation.model.FriendDTO
-import com.example.p2glet_sns.navigation.util.FcmPush
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -214,6 +210,7 @@ import kotlinx.android.synthetic.main.activity_add_photo.*
 import kotlinx.android.synthetic.main.activity_chat.*
 import kotlinx.android.synthetic.main.activity_comment.*
 import kotlinx.android.synthetic.main.item_chat.*
+import kotlinx.android.synthetic.main.item_chat.view.*
 import kotlinx.android.synthetic.main.item_comment.view.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -241,9 +238,9 @@ class ChatActivity : AppCompatActivity() {
 
         messageActivity_ImageView.setOnClickListener {
             Log.d("클릭 시 dest", "$destinationUid")
-            val chatModel = ChatDTO()
-            chatModel.users.put(uid!!, true)
-            chatModel.users.put(destinationUid!!, true)
+            var chatModel = ChatDTO().users
+            chatModel.put(uid!!, true)
+            chatModel.put(destinationUid!!, true)
 //            Log.d("클릭 시 uid", chatModel.users.toString())
             Log.d("클릭 시 curtime1", curTime)
             Log.d("클릭 시 curtime2", curTime.toString())
@@ -257,7 +254,7 @@ class ChatActivity : AppCompatActivity() {
                     checkChatRoom()
                     //메세지 보내기
                     Handler().postDelayed({
-                        println(chatRoomUid)
+//                        println(chatRoomUid)
                         fireDatabase.child("chatrooms").child(chatRoomUid.toString()).child("comments").push().setValue(comment)
                         messageActivity_editText.text = null
                     }, 1000L)
@@ -292,30 +289,20 @@ class ChatActivity : AppCompatActivity() {
                 })
     }
 
-//    fun commentAlarm(destinationUid : String, message : String) {
-//        var alarmDTO = AlarmDTO()
-//        alarmDTO.destinationUid = destinationUid
-//        alarmDTO.userId = FirebaseAuth.getInstance().currentUser?.email
-//        alarmDTO.kind = 1
-//        alarmDTO.uid = FirebaseAuth.getInstance().currentUser?.uid
-//        alarmDTO.timestamp = System.currentTimeMillis()
-//        alarmDTO.message = message
-//        FirebaseFirestore.getInstance().collection("alarms").document().set(alarmDTO)
-//
-//        var msg = FirebaseAuth.getInstance().currentUser?.email + " " + getString(R.string.alarm_comment) + " of " + message
-//        FcmPush.instance.sendMessage(destinationUid, "p2glet_sns", msg)
-//    }
-    inner class CommentRecyclerviewAdapter : RecyclerView.Adapter<CommentRecyclerviewAdapter.MessageViewHolder>() {
+    inner class CommentRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
         var comments : ArrayList<ChatDTO.Comment> = arrayListOf()
+        var contentDTO = ContentDTO()
+
         init {
             fireDatabase.child("users").child(destinationUid.toString()).addListenerForSingleValueEvent(object : ValueEventListener{
                 override fun onCancelled(error: DatabaseError) {
                 }
 
                 override fun onDataChange(snapshot: DataSnapshot) {
-                    friend = snapshot.getValue<FriendDTO>()
-                    messageActivity_textView_topName.text = friend?.name
+//                    cont = snapshot.getValue<ContentDTO>()
+                    contentDTO.userId = FirebaseAuth.getInstance().currentUser?.email
+                    messageActivity_textView_topName.text = contentDTO.userId
                     getMessageList()
                 }
             })
@@ -344,40 +331,51 @@ class ChatActivity : AppCompatActivity() {
             return MessageViewHolder(view)
         }
 
-        private inner class CustomViewHolder(view : View) : RecyclerView.ViewHolder(view)
+//        private inner class CustomViewHolder(view : View) : RecyclerView.ViewHolder(view)
 
         @SuppressLint("RtlHardcoded")
-        override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
-        Log.d("클릭 시 comments", comments[position].message.toString())
-        Log.d("클릭 시 time", comments[position].time.toString())
-            holder.textView_message?.textSize = 20F
-            holder.textView_message?.text = comments[position].message
-            holder.textView_time?.text = comments[position].time
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var holder = (holder as MessageViewHolder).itemView
+            var contentDTO : ArrayList<ContentDTO> = arrayListOf()
+            contentDTO.clear()
+
+            Log.d("클릭 시 comments", comments[position].message.toString())
+            Log.d("클릭 시 time", comments[position].time.toString())
+            holder.messageItem_textView_message?.textSize = 20F
+            holder.messageItem_textView_message?.text = comments[position].message
+            holder.messageItem_textView_time?.text = comments[position].time
+//            holder.messageItem_textview_name.text = contentDTO!![position].userId
             if(comments[position].uid.equals(uid)){ // 본인 채팅
-//                holder.textView_message.setBackgroundResource(R.drawable.rightbubble)
-                holder.textView_message?.visibility = View.INVISIBLE
-                holder.textView_name?.visibility = View.INVISIBLE
-                holder.layout_main?.gravity = Gravity.RIGHT
+                holder.messageItem_textView_message?.setBackgroundResource(R.drawable.rightbubble)
+                holder.messageItem_textView_message?.visibility = View.VISIBLE
+                holder.messageItem_textview_name?.visibility = View.VISIBLE
+//                holder.textView_time?.visibility = View.GONE
+                holder.messageItem_linearlayout_main?.gravity = Gravity.RIGHT
             }else{ // 상대방 채팅
-                Glide.with(holder.itemView.context)
-                    .load(friend?.profileImageUrl)
-                    .apply(RequestOptions().circleCrop())
-                    .into(messageItem_imageview_profile)
-                holder.textView_name?.text = friend?.name
-                holder.layout_destination?.visibility = View.VISIBLE
-                holder.textView_name?.visibility = View.VISIBLE
-//                holder.textView_message.setBackgroundResource(R.drawable.leftbubble)
-                holder.layout_main?.gravity = Gravity.LEFT
+//                Glide.with(holder.itemView.context).load(contentDTO.imageUrl).apply(RequestOptions().circleCrop()).into(messageItem_imageview_profile)
+
+                FirebaseFirestore.getInstance().collection("profileImages").document(comments[position].uid!!).get().addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        var url = task.result!!["image"]
+                        Glide.with(holder.context).load(url).apply(RequestOptions().circleCrop()).into(messageItem_imageview_profile)
+                    }
+                }
+
+                holder.messageItem_textview_name?.text = contentDTO[position].userId
+                holder.messageItem_layout_destination?.visibility = View.VISIBLE
+                holder.messageItem_textview_name?.visibility = View.VISIBLE
+                holder.messageItem_textView_message?.setBackgroundResource(R.drawable.leftbubble)
+                holder.messageItem_linearlayout_main?.gravity = Gravity.LEFT
             }
         }
     inner class MessageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            var textView_message: TextView? = view.findViewById(R.id.messageItem_textView_message)
-            var textView_name: TextView? = view.findViewById(R.id.messageItem_textview_name)
-//            var imageView_profile: ImageView = view.findViewById(R.id.messageItem_imageview_profile)
-            var layout_destination: LinearLayout? = view.findViewById(R.id.messageItem_layout_destination)
-
-            var layout_main: LinearLayout? = view.findViewById(R.id.messageItem_linearlayout_main)
-            var textView_time : TextView? = view.findViewById(R.id.messageItem_textView_time)
+//            var textView_message: TextView? = view.findViewById(R.id.messageItem_textView_message)
+//            var textView_name: TextView? = view.findViewById(R.id.messageItem_textview_name)
+////            var imageView_profile: ImageView = view.findViewById(R.id.messageItem_imageview_profile)
+//            var layout_destination: LinearLayout? = view.findViewById(R.id.messageItem_layout_destination)
+//
+//            var layout_main: LinearLayout? = view.findViewById(R.id.messageItem_linearlayout_main)
+//            var textView_time : TextView? = view.findViewById(R.id.messageItem_textView_time)
         }
 
         override fun getItemCount(): Int {
