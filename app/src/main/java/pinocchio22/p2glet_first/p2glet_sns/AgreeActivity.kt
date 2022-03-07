267e6e2e8a2200227ad05ffa92e3f0026a96c16c
pinocchio22 <vviian.2@gmail.com>
2022-03-07 21:27:31 +0900

    feat : AgreeActivity.kt

--- app/src/main/java/pinocchio22/p2glet_first/p2glet_sns/AgreeActivity.kt
+++ app/src/main/java/pinocchio22/p2glet_first/p2glet_sns/AgreeActivity.kt
@@ -2,36 +2,76 @@ package pinocchio22.p2glet_first.p2glet_sns
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatActivity
 import com.p2glet_first.p2glet_sns.R
 import kotlinx.android.synthetic.main.activity_agree.*
+import kotlin.system.exitProcess
+
 
 class AgreeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agree)
 
-        allCheckBtn.setOnClickListener { onCheckChanged(allCheckBtn) }
-        firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn) }
-        secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn) }
+//        allCheckBtn.setOnClickListener { onCheckChanged(allCheckBtn) }
+//        firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn) }
+//        secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn) }
+//
+//        firstCheckBtn_add.setOnClickListener {
+//            // 서비스 이용약관
+//            var intent = Intent(
+//                Intent.ACTION_VIEW,
+//                Uri.parse("https://velog.io/@pinocchio22/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%9D%B4%EC%9A%A9-%EC%95%BD%EA%B4%80")
+//            )
+//            startActivity(intent)
+//        }
+//        secondCheckBtn_add.setOnClickListener {
+//            // 개인정보처리지침
+//            var intent = Intent(
+//                Intent.ACTION_VIEW,
+//                Uri.parse("https://velog.io/@pinocchio22/%EA%B0%9C%EC%9D%B8%EC%A0%95%EB%B3%B4%EC%B2%98%EB%A6%AC%EB%B0%A9%EC%B9%A8")
+//            )
+//            startActivity(intent)
+//        }
 
-        firstCheckBtn_add.setOnClickListener {
-            // 서비스 이용약관
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://velog.io/@pinocchio22/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%9D%B4%EC%9A%A9-%EC%95%BD%EA%B4%80"))
-            startActivity(intent)
-        }
-        secondCheckBtn_add.setOnClickListener {
-            // 개인정보처리지침
-            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://velog.io/@pinocchio22/%EA%B0%9C%EC%9D%B8%EC%A0%95%EB%B3%B4%EC%B2%98%EB%A6%AC%EB%B0%A9%EC%B9%A8"))
-            startActivity(intent)
-        }
+        val pref = getSharedPreferences("checkFirst", MODE_PRIVATE)
+        val checkFirst = pref.getBoolean("checkFirst", false)
+        if (!checkFirst) {
+            // 앱 최초 실행
+            val editor = pref.edit()
+            editor.putBoolean("checkFirst", true)
+            editor.apply()
 
-        if (firstCheckBtn.isChecked && secondCheckBtn.isChecked) {
-            next_Btn.isEnabled
-        }
+            allCheckBtn.setOnClickListener { onCheckChanged(allCheckBtn) }
+            firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn) }
+            secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn) }
 
+            firstCheckBtn_add.setOnClickListener {
+                // 서비스 이용약관
+                var intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://velog.io/@pinocchio22/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%9D%B4%EC%9A%A9-%EC%95%BD%EA%B4%80")
+                )
+                startActivity(intent)
+            }
+            secondCheckBtn_add.setOnClickListener {
+                // 개인정보처리지침
+                var intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://velog.io/@pinocchio22/%EA%B0%9C%EC%9D%B8%EC%A0%95%EB%B3%B4%EC%B2%98%EB%A6%AC%EB%B0%A9%EC%B9%A8")
+                )
+                startActivity(intent)
+            }
+
+        } else {
+            // 앱 최초 실행x
+//            Log.d("또실행", checkFirst.toString())
+            finish()
+        }
     }
 
     private fun onCheckChanged(compoundButton: CompoundButton) {
@@ -40,7 +80,7 @@ class AgreeActivity : AppCompatActivity() {
                 if (allCheckBtn.isChecked) {
                     firstCheckBtn.isChecked = true
                     secondCheckBtn.isChecked = true
-                }else {
+                } else {
                     firstCheckBtn.isChecked = false
                     secondCheckBtn.isChecked = false
                 }
@@ -50,6 +90,10 @@ class AgreeActivity : AppCompatActivity() {
                         firstCheckBtn.isChecked
                                 && secondCheckBtn.isChecked)
             }
+        }
+        next_Btn_positive.isEnabled = firstCheckBtn.isChecked && secondCheckBtn.isChecked
+        next_Btn_positive.setOnClickListener {
+            finish()
         }
     }
 }
--- app/src/main/java/pinocchio22/p2glet_first/p2glet_sns/MainActivity.kt
+++ app/src/main/java/pinocchio22/p2glet_first/p2glet_sns/MainActivity.kt
@@ -2,19 +2,15 @@ package pinocchio22.p2glet_first.p2glet_sns
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.google.android.gms.tasks.OnCompleteListener
-import pinocchio22.p2glet_first.p2glet_sns.navigation.*
 import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
@@ -24,6 +20,7 @@ import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.UploadTask
 import com.p2glet_first.p2glet_sns.R
 import kotlinx.android.synthetic.main.activity_main.*
+import pinocchio22.p2glet_first.p2glet_sns.navigation.*
 
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -32,33 +29,47 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         when(item.itemId) {
             R.id.action_home -> {
                 var detailViewFragment = DetailViewFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content,detailViewFragment).commit()
+                supportFragmentManager.beginTransaction().replace(
+                    R.id.main_content,
+                    detailViewFragment
+                ).commit()
                 return true
             }
             R.id.action_search -> {
                 var gridFragment = GridFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content,gridFragment).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment)
+                    .commit()
                 return true
             }
             R.id.action_add_photo -> {
-                if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED
+                )
                     startActivity(Intent(this, AddPhotoActivity::class.java))
-                else ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
-                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                else ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    1
+                )
+                startActivity(Intent(this, AddPhotoActivity::class.java))
                 return true
             }
             R.id.action_favorite_alarm -> {
                 var alarmFragment = AlarmFragment()
-                supportFragmentManager.beginTransaction().replace(R.id.main_content,alarmFragment).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment)
+                    .commit()
                 return true
             }
             R.id.action_account -> {
                 var userFragment = UserFragment()
                 var bundle = Bundle()
                 var uid = FirebaseAuth.getInstance().currentUser?.uid
-                bundle.putString("destinationUid",uid)
+                bundle.putString("destinationUid", uid)
                 userFragment.arguments = bundle
-                supportFragmentManager.beginTransaction().replace(R.id.main_content,userFragment).commit()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment)
+                    .commit()
                 return true
             }
         }
@@ -89,12 +100,16 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, AgreeActivity::class.java))
+
         bottom_navigation.setOnNavigationItemSelectedListener(this)
 //        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1)
 
         //Set default screen
         bottom_navigation.selectedItemId = R.id.action_home
         registerPushToken()
+//        first_start()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -103,14 +118,32 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         if (requestCode == UserFragment.PICK_PROFILE_FROM_ALBUM && resultCode == Activity.RESULT_OK) {
             var imageUri = data?.data
             var uid = FirebaseAuth.getInstance().currentUser?.uid
-            var storageRef = FirebaseStorage.getInstance().reference.child("userProfileImages").child(uid!!)
-            storageRef.putFile(imageUri!!).continueWithTask { task : Task<UploadTask.TaskSnapshot> ->
+            var storageRef = FirebaseStorage.getInstance().reference.child("userProfileImages").child(
+                uid!!
+            )
+            storageRef.putFile(imageUri!!).continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
                 return@continueWithTask storageRef.downloadUrl
             }.addOnSuccessListener { uri ->
-                var map = HashMap<String,Any>()
+                var map = HashMap<String, Any>()
                 map["image"] = uri.toString()
                 FirebaseFirestore.getInstance().collection("profileImages").document(uid).set(map)
             }
         }
     }
+//    fun first_start() {
+//        val pref = getSharedPreferences("checkFirst", MODE_PRIVATE)
+//        val checkFirst = pref.getBoolean("checkFirst", false)
+//        if (!checkFirst) {
+//            // 앱 최초 실행
+//            val editor = pref.edit()
+//            editor.putBoolean("checkFirst", true)
+//            editor.apply()
+//            Log.d("첫실행", checkFirst.toString())
+//            startActivity(Intent(this, AgreeActivity::class.java))
+//
+//        } else {
+//            // 앱 최초 실행x
+//            Log.d("또실행", checkFirst.toString())
+//        }
+//    }
 }
