feed89d3f5209e8562749927a094c75edf246e8e
pinocchio22 <vviian.2@gmail.com>
2022-03-07 17:39:23 +0900

    feat : AgreeActivity.kt
    added click event

--- app/src/main/java/pinocchio22/p2glet_first/p2glet_sns/AgreeActivity.kt
+++ app/src/main/java/pinocchio22/p2glet_first/p2glet_sns/AgreeActivity.kt
@@ -1,5 +1,7 @@
 package pinocchio22.p2glet_first.p2glet_sns
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.CompoundButton
@@ -14,6 +16,17 @@ class AgreeActivity : AppCompatActivity() {
         allCheckBtn.setOnClickListener { onCheckChanged(allCheckBtn) }
         firstCheckBtn.setOnClickListener { onCheckChanged(firstCheckBtn) }
         secondCheckBtn.setOnClickListener { onCheckChanged(secondCheckBtn) }
+
+        firstCheckBtn_add.setOnClickListener {
+            // 서비스 이용약관
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://velog.io/@pinocchio22/%EC%84%9C%EB%B9%84%EC%8A%A4-%EC%9D%B4%EC%9A%A9-%EC%95%BD%EA%B4%80"))
+            startActivity(intent)
+        }
+        secondCheckBtn_add.setOnClickListener {
+            // 개인정보처리지침
+            var intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://velog.io/@pinocchio22/%EA%B0%9C%EC%9D%B8%EC%A0%95%EB%B3%B4%EC%B2%98%EB%A6%AC%EB%B0%A9%EC%B9%A8"))
+            startActivity(intent)
+        }
 
     }
 
